b7c4b33b7172efede4196850733362bf9bafef44
feiyy <feiyy@neusoft.com>
2020-05-27 14:37:44 +0800

    add axios api

--- js/virus.js
+++ js/virus.js
@@ -1,4 +1,13 @@
 var app = new Vue({
 	el:"#app",
-	data:{}
+	data:{
+		newslist:[]
+	},
+	created(){
+		axios.get('http://api.tianapi.com/txapi/ncovcity/index?key=229a8745b39d447b656d775df952443c')
+		.then(res => {
+			console.log(res);
+			this.newslist = res.data.newslist;
+		})	
+	}
 })
