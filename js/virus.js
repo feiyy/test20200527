29af6f04230febbb6656c113a1da625374bebe71
feiyy <feiyy@neusoft.com>
2020-05-27 14:56:42 +0800

    toggle read more

--- js/virus.js
+++ js/virus.js
@@ -1,7 +1,29 @@
 var app = new Vue({
 	el:"#app",
 	data:{
+		message:'read more',
+		flag:false,//display 6 province
 		newslist:[]
+	},
+	computed:{
+		displaylist()
+		{
+			if(!this.flag)
+			{
+				this.message = 'read more';
+				var temp = [];
+				for(var i=0; i<6;i++)
+				{
+					temp.push(this.newslist[i])
+				}
+				return temp;
+			}
+			else
+			{
+				this.message ='read less';
+				return this.newslist;
+			}
+		}
 	},
 	created(){
 		axios.get('http://api.tianapi.com/txapi/ncovcity/index?key=229a8745b39d447b656d775df952443c')
